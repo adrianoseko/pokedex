826cb777d541ee6ab3f8bbfb559299744ee7d6bb
Adriano Jorge de Moraes <68881626+adrianoseko@users.noreply.github.com>
2026-03-19 21:49:14 -0300

    refactor: pokedex-frontend/src/app/app.module.ts via HighDev.AI

--- pokedex-frontend/src/app/app.module.ts
+++ pokedex-frontend/src/app/app.module.ts
@@ -1,31 +1,49 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Type } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+
 import { AppComponent } from './app.component';
 import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
 import { PokemonDetailComponent } from './pokemon-detai/pokemon-detai.component';
-import { ButtonModule } from 'primeng/button';
-import { CardModule } from 'primeng/card';
 import { PokedexComponent } from './pokedex/pokedex.component';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    PokemonListComponent,
-    PokemonDetailComponent,
-    PokedexComponent,
+/**
+ * Root application module.
+ *
+ * Declarations and imports are grouped into well-named constants to improve
+ * readability, maintenance and to make future testing/extensions easier.
+ */
+const DECLARATIONS: Array<Type<any>> = [
+  AppComponent,
+  PokemonListComponent,
+  PokemonDetailComponent,
+  PokedexComponent
+];
 
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    TableModule,
-    HttpClientModule,
-    ButtonModule,
-    CardModule
-  ],
+const FRAMEWORK_MODULES: Array<any> = [
+  BrowserModule,
+  AppRoutingModule,
+  HttpClientModule
+];
+
+const UI_MODULES: Array<any> = [
+  TableModule,
+  ButtonModule,
+  CardModule
+];
+
+const IMPORTS: Array<any> = [
+  ...FRAMEWORK_MODULES,
+  ...UI_MODULES
+];
+
+@NgModule({
+  declarations: DECLARATIONS,
+  imports: IMPORTS,
   providers: [],
   bootstrap: [AppComponent]
 })
