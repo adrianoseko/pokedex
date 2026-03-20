cefd7a54982fbf767ba020e751cc11a46df0f9e4
Adriano Jorge de Moraes <68881626+adrianoseko@users.noreply.github.com>
2026-03-19 21:49:14 -0300

    refactor: pokedex-frontend/karma.conf.js via HighDev.AI

--- pokedex-frontend/karma.conf.js
+++ pokedex-frontend/karma.conf.js
@@ -1,23 +1,39 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
+'use strict';
 
+/**
+ * Karma configuration factory
+ * @param {Object} config - Karma configuration object provided by Karma runner
+ */
 module.exports = function (config) {
+  if (!config || typeof config.set !== 'function') {
+    throw new TypeError('Expected a Karma config object with a set(config) method.');
+  }
+
+  const path = require('path');
+
+  // Constants for readability and easy modification
+  const PORT = 9876;
+  const BROWSERS = ['Chrome'];
+  const FRAMEWORKS = ['jasmine', '@angular-devkit/build-angular'];
+  const PLUGINS = [
+    require('karma-jasmine'),
+    require('karma-chrome-launcher'),
+    require('karma-jasmine-html-reporter'),
+    require('karma-coverage'),
+    require('@angular-devkit/build-angular/plugins/karma')
+  ];
+
+  const coverageOutputDir = path.join(__dirname, './coverage/pokedex-frontend');
+  const COVERAGE_REPORTERS = [{ type: 'html' }, { type: 'text-summary' }];
+
   config.set({
     basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
-    plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage'),
-      require('@angular-devkit/build-angular/plugins/karma')
-    ],
+    frameworks: FRAMEWORKS,
+    plugins: PLUGINS,
     client: {
       jasmine: {
-        // you can add configuration options for Jasmine here
-        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
-        // for example, you can disable the random execution with `random: false`
-        // or set a specific seed with `seed: 4321`
+        // Add Jasmine configuration here if needed
+        // See: https://jasmine.github.io/api/edge/Configuration.html
       },
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
@@ -25,19 +41,16 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/pokedex-frontend'),
+      dir: coverageOutputDir,
       subdir: '.',
-      reporters: [
-        { type: 'html' },
-        { type: 'text-summary' }
-      ]
+      reporters: COVERAGE_REPORTERS
     },
     reporters: ['progress', 'kjhtml'],
-    port: 9876,
+    port: PORT,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: BROWSERS,
     singleRun: false,
     restartOnFileChange: true
   });
